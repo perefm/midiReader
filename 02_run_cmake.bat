a90ffe9b645314260e17980c6b19c57808b7a18a
xphere <xpherezn@gmail.com>
2025-11-20 22:57:27 +0100

    Upgrade to VS 2026

--- 02_run_cmake.bat
+++ 02_run_cmake.bat
@@ -1,7 +1,7 @@
-rmdir midiReader_vs2022 /s /q
-mkdir midiReader_vs2022
-cd midiReader_vs2022
-cmake.exe -DVCPKG_TARGET_TRIPLET=x64-windows-static -G "Visual Studio 17 2022" ..
+rmdir midiReader_vs2026 /s /q
+mkdir midiReader_vs2026
+cd midiReader_vs2026
+cmake.exe -DVCPKG_TARGET_TRIPLET=x64-windows-static -G "Visual Studio 18 2026" ..
 cd..
 
 pause
